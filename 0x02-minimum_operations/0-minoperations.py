b47dab2d7912c9c0e6df8f4c96adaf94ddf96125
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-17 22:10:44 +0000

    Trying to debug

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Minimum operations problem"""
+"""Minimum operations problem."""
 
 
 def minOperations(n: int) -> int:
-    """Finds minimum number of copyAll and paste operations to reach n
+    """Find minimum number of copyAll and paste operations to reach n.
 
     Args:
-        n (int): Target character count
+        n (int): Target character count.
 
     Returns:
-        int: the number of minimum operations to reach n
+        int: the number of minimum operations to reach n.
     """
     if n < 0:
         return 0
@@ -18,7 +18,7 @@ def minOperations(n: int) -> int:
 
     for i in range(2, n + 1):
         min_ops[i] = i
-        
+
         for j in range(1, (i // 2) + 1):
             if i % j == 0:
                 min_ops[i] = min(min_ops[i], min_ops[j] + (i // j))
