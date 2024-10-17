1247bc6f8bee9fd76165e60a16ef61c90ea1e009
Daniel Abayomi <dan234bayomi@gmail.com>
2024-10-17 22:05:01 +0000

    Initial commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Minimum operations problem"""
+
+
+def minOperations(n: int) -> int:
+    """Finds minimum number of copyAll and paste operations to reach n
+
+    Args:
+        n (int): Target character count
+
+    Returns:
+        int: the number of minimum operations to reach n
+    """
+    if n < 0:
+        return 0
+
+    min_ops = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        min_ops[i] = i
+        
+        for j in range(1, (i // 2) + 1):
+            if i % j == 0:
+                min_ops[i] = min(min_ops[i], min_ops[j] + (i // j))
+
+    return min_ops[n]
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,2 @@
+## Min Operations Problem
+Use of dynamic programming in se interview based questions
