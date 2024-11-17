8f4acfe710b21f137c01a65c54a21da482fcbeea
Daniel Abayomi <dan234bayomi@gmail.com>
2024-11-17 21:18:10 +0100

    Giving up

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,21 @@
 #!/usr/bin/node
-/* eslint-disable no-console */
 const util = require('util');
 const request = util.promisify(require('request'));
 
-const movieId = Number(process.argv[2]);
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
-function getCharacterName(characterUrl, callback) {
-  request(characterUrl, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-      callback(null);
-    } else {
-      const data = JSON.parse(body);
-      callback(data.name);
-    }
-  });
+async function starwarsCharacters() {
+  const endpoint = `${API_URL}/films/${process.argv[2]}/`;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const {characters} = response;
+
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
 }
 
-function fetchAndPrintCharacters() {
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error('Error:', error);
-      return;
-    }
-
-    const data = JSON.parse(body);
-    const characterUrls = Array.from(data.characters);
-
-    let completedRequests = 0;
-    const characterNames = [];
-
-    characterUrls.forEach((link, index) => {
-      getCharacterName(link, (name) => {
-        if (name) {
-          characterNames[index] = name;
-        }
-        // eslint-disable-next-line no-plusplus
-        completedRequests++;
-
-        if (completedRequests === characterUrls.length) {
-          // Print character names in the order of the original requests
-          characterNames.forEach((character) => {
-            console.log(character);
-          });
-        }
-      });
-    });
-  });
-}
-
-fetchAndPrintCharacters();
+starwarsCharacters();
